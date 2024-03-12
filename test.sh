91228e695b9b25e9aceed6f6bc37bd6dc1f93339
Marco Solis <this.solis@ieee.org>
2024-03-11 18:37:52 -0600

    update tests

--- test.sh
+++ test.sh
@@ -41,7 +41,7 @@ echo "user1 address" $user1_address
 echo "user2 address" $user2_address
 
 # use owner wallet to load/install an Ghost sc to chain
-ghostscid=$(curl --silent --request POST --data-binary   @ghost.bas http://127.0.0.1:$owner_rpc_port/install_sc| jq -r ".txid")
+ghostscid=$(curl --silent --request POST --data-binary   @ghost_new.bas http://127.0.0.1:$owner_rpc_port/install_sc| jq -r ".txid")
 echo "Ghost SCID" $ghostscid
 sleep 2
 
@@ -54,12 +54,11 @@ asset2scid=$(curl --silent --request POST --data-binary   @asset.bas http://127.
 echo "asset2 SCID" $asset2scid
 sleep 2
 
-
-echo -n "user 1 minting 100000 asset1 "
+echo -n "user 1 minting 20000000000 asset1 "
 curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":100000,"scid":"'"$asset1scid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"IssueAsset"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
-echo -n "user 2 minting 100000 asset1 "
+echo -n "user 2 minting 20000000000 asset1 "
 curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":100000,"scid":"'"$asset1scid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"IssueAsset"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
@@ -89,11 +88,11 @@ sleep 2
 curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"transfer","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"AddLiquidity"},{"name":"asset_address","datatype":"S","value":"'"$asset1scid"'"},{"name":"min_liquidity","datatype":"U","value":1}], "transfers": [{"burn":1234,"destination":"'"$user1_address"'"},{"scid":"'"$asset1scid"'", "burn":5000}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
-echo -n "user 1 exchanging 100000 dero for asset2 "
+echo -n "user 1 minting 20000000000 asset2 "
 curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":100000,"scid":"'"$asset2scid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"IssueAsset"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
-echo -n "user 2 exchanging 100000 dero for asset2 "
+echo -n "user 2 minting 20000000000 asset2 "
 curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":100000,"scid":"'"$asset2scid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"IssueAsset"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
@@ -112,19 +111,19 @@ curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id
 sleep 2
 
 echo -n "Fails when user 1 swaps 1000 dero for asset2 and receives less than the min specified"
-curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":1000,"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"DeroToAssetSwapInputMin"},{"name":"min_assets","datatype":"U","value":402},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
-sleep 2
-
-echo -n "user 1 swaps 1000 dero for asset2 specifying min assets to receive"
 curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":1000,"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"DeroToAssetSwapInputMin"},{"name":"min_assets","datatype":"U","value":401},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
+echo -n "user 1 swaps 1000 dero for asset2 specifying min assets to receive"
+curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":1000,"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"DeroToAssetSwapInputMin"},{"name":"min_assets","datatype":"U","value":400},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+sleep 2
+
 echo -n "Fails when user 1 buys 100 asset2 and pays more dero than the max specified"
-curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":267,"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"DeroToAssetSwapOutput"},{"name":"assets_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":266,"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"DeroToAssetSwapOutput"},{"name":"assets_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
 echo -n "user 1 buys 100 asset2 specifying max dero to pay"
-curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":268,"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"DeroToAssetSwapOutput"},{"name":"assets_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"sc_dero_deposit":267,"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"DeroToAssetSwapOutput"},{"name":"assets_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
 echo -n "Fails when user 2 sells 100 asset2 and receives less than the min specified"
@@ -136,15 +135,30 @@ curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id
 sleep 2
 
 echo -n "fails when user 2 spends more than the max of asset2 specified for sell to receive a specific dero amount"
-curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"transfer","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"AssetToDeroSwapOutput"},{"name":"dero_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}], "transfers": [{"scid":"'"$asset2scid"'", "burn":38}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"transfer","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"AssetToDeroSwapOutput"},{"name":"dero_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}], "transfers": [{"scid":"'"$asset2scid"'", "burn":37}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
 echo -n "user 2 specifies a max of asset2 to sell to receive a specific dero amount"
-curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"transfer","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"AssetToDeroSwapOutput"},{"name":"dero_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}], "transfers": [{"scid":"'"$asset2scid"'", "burn":39}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"transfer","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"AssetToDeroSwapOutput"},{"name":"dero_bought","datatype":"U","value":100},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"}], "transfers": [{"scid":"'"$asset2scid"'", "burn":38}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+sleep 2
+
+echo -n "user 1 removing all liquidity for the asset1 pair"
+curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"RemoveLiquidity"},{"name":"asset_address","datatype":"S","value":"'"$asset1scid"'"},{"name":"amount","datatype":"U","value":10000},{"name":"min_dero","datatype":"U","value":1},{"name":"min_assets","datatype":"U","value":1}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+sleep 2
+
+echo -n "user 1 removing all liquidity for the asset2 pair"
+curl --silent http://127.0.0.1:$user1_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"RemoveLiquidity"},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"},{"name":"amount","datatype":"U","value":10000},{"name":"min_dero","datatype":"U","value":1},{"name":"min_assets","datatype":"U","value":1}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+sleep 2
+
+echo -n "user 2 removing all liquidity for the asset1 pair"
+curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"RemoveLiquidity"},{"name":"asset_address","datatype":"S","value":"'"$asset1scid"'"},{"name":"amount","datatype":"U","value":1234},{"name":"min_dero","datatype":"U","value":1},{"name":"min_assets","datatype":"U","value":1}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
+sleep 2
+
+echo -n "user 2 removing all liquidity for the asset1 pair"
+curl --silent http://127.0.0.1:$user2_rpc_port/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"scinvoke","params":{"scid":"'"$ghostscid"'","ringsize":2, "sc_rpc":[{"name":"entrypoint","datatype":"S","value":"RemoveLiquidity"},{"name":"asset_address","datatype":"S","value":"'"$asset2scid"'"},{"name":"amount","datatype":"U","value":1234},{"name":"min_dero","datatype":"U","value":1},{"name":"min_assets","datatype":"U","value":1}] }}' -H 'Content-Type: application/json' | jq -r ".result.txid"
 sleep 2
 
 echo "SC DERO balance" $(scassetbalance $ghostscid $baseasset )
 echo "SC Asset1 balance" $(scassetbalance $ghostscid $asset1scid )
 echo "SC Asset2 balance" $(scassetbalance $ghostscid $asset2scid )
-
 exit 0
