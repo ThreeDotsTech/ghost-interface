dd62cde7ba24313fbff11ea2d9eb8f871199b4c1
Marco Solis <this.solis@ieee.org>
2024-06-23 13:40:46 -0600

    make Ghost Exchange legend responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <header className="fixed top-0 left-0 w-full p-8">
       <div className="relative min-h-10 w-full flex justify-between items-center bg-background text-text bg-white border-2 border-black rounded-xl px-5">
-        <div className="absolute font-bold text-lg left-1/2 transform -translate-x-1/2 my-2">Ghost Exchange</div>
+        <div className="absolute font-bold text-lg ml-5 md:ml-0 left-0 md:left-1/2 transform md:-translate-x-1/2 my-2">Ghost Exchange</div>
         <div className="absolute right-5 wallet-info flex items-center pr-3">
           { connectionType === 'XSWD' ? 
             <WalletInfoSection/>
